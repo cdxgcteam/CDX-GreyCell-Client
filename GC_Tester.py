53e651dc018c54f26fc2c1374c1c5f418771ff6a
je106 <james.r.elliott106.civ@mail.mil>
2015-04-08 16:18:00 -0400

    Tester: Catch exceptions
    exit when exceptions are caught

--- GC_Tester.py
+++ GC_Tester.py
@@ -2,64 +2,66 @@ from GCClient import GCClient
 import GC_Utility
 import json
 import time
+try:
+	instance = GCClient(debug = True, enable_comms = False)
 
-instance = GCClient(debug = True, enable_comms = True)
+	taskObj = {}
+	command = {}
 
-taskObj = {}
-command = {}
+	taskObj['TaskId'] = 'ABC123'
+	taskObj['routingKey'] = 'GREYUNI.GREYTEST'
+	taskObj[GC_Utility.GC_TASKREF] = 'Ref123'
 
-taskObj['TaskId'] = 'ABC123'
-taskObj['routingKey'] = 'GREYUNI.GREYTEST'
-taskObj[GC_Utility.GC_TASKREF] = 'Ref123'
+	taskObj[GC_Utility.GC_MODULEID] = 'selenium'
+	command['cmd'] = 'execute_url'
+	command['url'] = 'https://www.cnn.com'
+	command['timer'] = 10
 
-taskObj[GC_Utility.GC_MODULEID] = 'selenium'
-command['cmd'] = 'execute_url'
-command['url'] = 'https://www.cnn.com'
-command['timer'] = 10
+	message = """From: CDX Test <CDXTest779@gmail.com>
+	To: To Person <jim@elliott-family.net>
+	MIME-Version: 1.0
+	Content-type: text/html
+	Subject: SMTP HTML e-mail test
 
-message = """From: CDX Test <CDXTest779@gmail.com>
-To: To Person <jim@elliott-family.net>
-MIME-Version: 1.0
-Content-type: text/html
-Subject: SMTP HTML e-mail test
+	This is an e-mail message to be sent in HTML format
 
-This is an e-mail message to be sent in HTML format
+	<b>This is HTML message.</b>
+	<h1>This is headline.</h1>
+	"""
+	# taskObj[GC_Utility.GC_MODULEID] = 'email'
+	# command['cmd'] = 'sendemail'
+	# command['msg'] = message
+	# command['sender'] = 'CDXTest779@gmail.com'
+	# command['receivers'] = 'jim@elliott-family.net'
 
-<b>This is HTML message.</b>
-<h1>This is headline.</h1>
-"""
-# taskObj[GC_Utility.GC_MODULEID] = 'email'
-# command['cmd'] = 'sendemail'
-# command['msg'] = message
-# command['sender'] = 'CDXTest779@gmail.com'
-# command['receivers'] = 'jim@elliott-family.net'
-
-# taskObj[GC_Utility.GC_MODULEID] = 'download'
-# command['url'] = 'http://the.earth.li/~sgtatham/putty/latest/x86/putty.exe'
-# command['saveas'] = '.\\7z938-extra.7z'
+	# taskObj[GC_Utility.GC_MODULEID] = 'download'
+	# command['url'] = 'http://the.earth.li/~sgtatham/putty/latest/x86/putty.exe'
+	# command['saveas'] = '.\\7z938-extra.7z'
 
 
-taskObj[GC_Utility.GC_CMD_DATA] = command
+	taskObj[GC_Utility.GC_CMD_DATA] = command
 
-instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
-taskObj[GC_Utility.GC_TASKREF] = 'Ref456'
-taskObj[GC_Utility.GC_CMD_DATA]['url'] = 'https://bitcoin.org/bitcoin.pdf'
+	instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
+	# taskObj[GC_Utility.GC_TASKREF] = 'Ref456'
+	# taskObj[GC_Utility.GC_CMD_DATA]['url'] = 'https://bitcoin.org/bitcoin.pdf'
 
-instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
+	# instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
 
-# taskObj[GC_Utility.GC_TASKREF] = 'Ref789'
-# taskObj[GC_Utility.GC_CMD_DATA]['url'] = 'http://127.0.0.1'
-# instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
+	# # taskObj[GC_Utility.GC_TASKREF] = 'Ref789'
+	# # taskObj[GC_Utility.GC_CMD_DATA]['url'] = 'http://127.0.0.1'
+	# # instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
 
-taskObj[GC_Utility.GC_MODULEID] = 'download'
-taskObj[GC_Utility.GC_CMD_DATA]['url'] = 'https://www.cnn.com'
-command['saveas'] = '.\\cnn_index.html'
-instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
+	# taskObj[GC_Utility.GC_MODULEID] = 'download'
+	# taskObj[GC_Utility.GC_CMD_DATA]['url'] = 'https://www.cnn.com'
+	# command['saveas'] = '.\\cnn_index.html'
+	# instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
 
-taskObj[GC_Utility.GC_MODULEID] = 'download'
-taskObj[GC_Utility.GC_CMD_DATA]['url'] = 'https://bitcoin.org/bitcoin.pdf'
-command['saveas'] = '.\\bitcoin.pdf'
-instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
+	# taskObj[GC_Utility.GC_MODULEID] = 'download'
+	# taskObj[GC_Utility.GC_CMD_DATA]['url'] = 'https://bitcoin.org/bitcoin.pdf'
+	# command['saveas'] = '.\\bitcoin.pdf'
+	# instance.logging_callback('ch', 'method', 'properties', json.dumps(taskObj))
 
-time.sleep(5*60)
-instance.quit()
+	# time.sleep(5*60)
+	instance.quit()
+except Exception as e:
+	print e
