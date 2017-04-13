e229a851ffc304a9106025c11a621dd55bc80a8e
je106 <james.elliott106.civ@mail.mil>
2017-04-13 13:38:48 -0400

    re-enable headless unix

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-Xvfb :99 &
-export DISPLAY=:99
+#Xvfb :99 &
+#export DISPLAY=:99
 export PATH=$PATH:../bin
 pypy run_client.py $1 $2 $3
-ps | grep Xvfb | cut -d ' ' -f1 | xargs kill
+#ps | grep Xvfb | cut -d ' ' -f1 | xargs kill
